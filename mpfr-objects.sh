e7975fa03fd31d757489a0ec30697b3a1e54283a
Faveo Hörold <faveo.hoerold@cscs.ch>
2025-07-13 00:42:20 +0200

    Replace remaining Enzyme names with Raptor.

--- mpfr-objects.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-cp ${BASE_PATH}/Enzyme/enzyme/include/enzyme/fprt/mpfr.h "./mpfr.cpp"
-clang++ -c "./mpfr.cpp" $(pkg-config --cflags mpfr gmp) \
-    -I${BASE_PATH}/Enzyme/enzyme/include/enzyme/fprt/ \
-    -o "./mpfr.o"
